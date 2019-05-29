ca30370de118f018c69024932dfbdfb35018b195
L1692 <franchiluigi92@yahoo.it>
2019-05-29 12:36:35 +0200

    Added project archetype

--- codedeploy/ApplicationStart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo $(aws ecr get-login --region eu-west-1 --no-include-email)
-/usr/local/bin/kubectl run cicdapp --image=195360077735.dkr.ecr.eu-west-1.amazonaws.com/cicdapp:latest --port=80
-/usr/local/bin/kubectl expose deployment cicdapp --type='LoadBalancer'
--- codedeploy/ApplicationStop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/kubectl delete services cicdapp
-/usr/local/bin/kubectl delete deployment cicdapp
-sudo docker rmi $(sudo docker images -f dangling=true -q)
